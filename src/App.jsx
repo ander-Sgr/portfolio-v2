41d6dfb91aa2c9bf68683bc9ec3521245ed21044
ander <andersonbarreto2001@gmail.com>
2025-04-26 17:52:19 +0200

    importing hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import Navbar from './components/layout/Navbar/Navbar'
+import Hero from './components/ui/hero/hero'
 import './App.css'
 
 function App() {
-
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
