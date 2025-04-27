ac39d2cb935db746d610c8c35f15db4522e1277f
ander <andersonbarreto2001@gmail.com>
2025-04-27 13:50:11 +0200

    rename file

---
+++ src/components/ui/hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from './Hero.module.css';
+
+const Hero = () => {
+    return (
+        <section className={styles.hero}>
+            <div className={styles.heroBackground}></div>
+
+            <div className={styles.heroContent}>
+                <h1>Hola, soy Anderson</h1>
+                <h2>Cloud & Devops Enthusiast</h2>
+                <p>
+                    Entusiasta de la tecnología, con <strong>experiencia en web development y cloud</strong>, actualmente
+                    estoy desarrollando mi carrera en Cloud, desarrollando proyectos en Python y desplegando
+                    con Terraform en AWS.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
