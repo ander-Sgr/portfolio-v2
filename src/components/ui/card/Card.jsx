4200313402b3b5feef86f7552ba76d28a365d86f
ander <andersonbarreto2001@gmail.com>
2025-04-30 15:20:16 +0200

    feat adding card ui for show projects

--- src/components/ui/card/Card.jsx
+++ src/components/ui/card/Card.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import styles from './Card.module.css'
 
-const Card = () => {
+const Card = ({ imgProject, titleProject, description, svgIcon = [], urlDemo, urlCode }) => {
     return (
-        <div></div>
+        <div className={styles.cardContainer}>
+            <div className={styles.imageContainer}>
+                <img src={imgProject} alt="img_project" className={styles.projectImage} />
+            </div>
+            <div className={styles.contentContainer}>
+                <h3 className={styles.projectTitle}>{titleProject}</h3>
+                <p className={styles.projectDescription}>{description}</p>
+                <div className={styles.techStack}>
+                    <div className={styles.techStack}>
+                        {svgIcon.map((icon, index) => (
+                            <span key={index}>{icon}</span>
+                        ))}
+                    </div>
+                </div>
+                <div className={styles.buttonsContainer}>
+                    <a className={styles.buttonLink} href={urlDemo} target="_blank" rel="noopener noreferrer">Demo</a>
+                    <a className={styles.buttonLink} href={urlCode} target="_blank" rel="noopener noreferrer">Code</a>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
