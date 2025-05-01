d7872dfc0a911d51c7c34d1f7bf0fbc1e5cd6b77
ander <andersonbarreto2001@gmail.com>
2025-05-01 17:33:46 +0200

    section projects done

--- src/components/pages/projects/Projects.jsx
+++ src/components/pages/projects/Projects.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import Card from "../../ui/card/Card";
-import { FaPython, FaAmazon } from 'react-icons/fa';
+import { FaPython, FaAmazon, FaCode } from 'react-icons/fa';
 import imgProject1 from './assets/ec2_manager.png';
 import styles from './Projects.module.css'
 
 const Projects = () => {
     return (
-        <div className={styles.cardContainer}>
-            <Card
-                imgProject={imgProject1}
-                titleProject="EC2 Manager"
-                description="Tool to manage EC2 instances easily."
-                svgIcon={[<FaPython key="python" />, <FaAmazon key="aws" />]}
-                urlDemo="https://demo.com"
-                urlCode="https://github.com"
-            />
-            <Card
-                imgProject={imgProject1}
-                titleProject="EC2 Manager"
-                description="Tool to manage EC2 instances easily."
-                svgIcon={[<FaPython key="python" title="Python" />, <FaAmazon key="aws" />]}
-                urlDemo="https://demo.com"
-                urlCode="https://github.com"
-            />
-        </div>
+        <section className={styles.projectsContainer}>
+            <h2 className={styles.projectTitle}>
+                <span><FaCode /></span>
+                Proyectos
+            </h2>
+            <p className={styles.descriptionProjects}>Algunos proyectos en los que he trabajado</p>
+            <div className={styles.cardContainer}>
+                <Card
+                    imgProject={imgProject1}
+                    titleProject="EC2 Manager"
+                    description="Herramienta de CLI para gestionar EC2 en aws, fue realizado con fines practicos, 
+                    puedes apagar, detenerlas."
+                    svgIcon={[<FaPython key="Python" />, <FaAmazon key="aws" />]}
+                    urlDemo="https://demo.com"
+                    urlCode="https://github.com"
+                />
+                
+                <Card
+                    imgProject={imgProject1}
+                    titleProject="EC2 Manager"
+                    description="Tool to manage EC2 instances easily."
+                    svgIcon={[<FaPython key="python" title="Python" />, <FaAmazon key="aws" />]}
+                    urlDemo="https://demo.com"
+                    urlCode="https://github.com"
+                />
+
+            </div>
+        </section>
+
     );
 };
 
