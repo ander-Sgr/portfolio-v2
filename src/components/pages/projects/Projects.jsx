8172962c18895c7e132ca05dc0fbd7e46a42c75c
ander <andersonbarreto2001@gmail.com>
2025-05-04 00:35:27 +0200

    change styles section

--- src/components/pages/projects/Projects.jsx
+++ src/components/pages/projects/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "../../ui/card/Card";
 import { FaPython, FaAmazon, FaCode } from 'react-icons/fa';
 import imgProject1 from './assets/ec2_manager.png';
+import imgProejct2 from './assets/aws_arquitectura.png'
 import styles from './Projects.module.css'
 
 const Projects = () => {
@@ -22,10 +23,10 @@ const Projects = () => {
                     urlDemo="https://demo.com"
                     urlCode="https://github.com"
                 />
-                
+
                 <Card
-                    imgProject={imgProject1}
-                    titleProject="EC2 Manager"
+                    imgProject={imgProejct2}
+                    titleProject="Deploy Infra con terraform"
                     description="Tool to manage EC2 instances easily."
                     svgIcon={[<FaPython key="python" title="Python" />, <FaAmazon key="aws" />]}
                     urlDemo="https://demo.com"
